6eebd8d16da366a98e450f430b31d54d34c03129
Matthew Spofford <matthewspofford@verizon.net>
2020-08-16 17:25:12 -0400

    Reresolved #9: SystemInfo has been replaced with JNA's Platform utility, and UsageCheckableEnum has been removed as a result

--- src/main/java/edu/wpi/surflab/utils/ResourceLoader.java
+++ src/main/java/edu/wpi/surflab/utils/ResourceLoader.java
@@ -1,7 +1,6 @@
 package edu.wpi.surflab.utils;
 
-import edu.wpi.surflab.utils.SystemInfo.ArchType;
-import edu.wpi.surflab.utils.SystemInfo.OSType;
+import com.sun.jna.Platform;
 import java.io.IOException;
 import java.util.Objects;
 import javafx.fxml.FXMLLoader;
@@ -15,22 +14,28 @@ import org.jetbrains.annotations.NotNull;
  */
 public final class ResourceLoader {
 
+  /**
+   * Base location for all application resources.
+   */
   public static final String BASE_RESOURCE_PATH = "edu/wpi/surflab/";
+  /**
+   * Defines location for SurfAPI in resources.
+   */
   public static final String SURF_API_PATH;
-
-  // Paths used to construct SURF_API_PATH
-  private static final String SURF_API_BASE = "utils/surfapi/";
-  private static final String SURF_API_x64 = "x64/";
-  private static final String SURF_API_x86 = "Win32/";
+  /**
+   * Defines name of SurfAPI in resources.
+   */
+  public static final String SURF_API = "surfapi.dll";
 
   // Initialize SURF_API_PATH
   static {
+    // Paths used to construct SURF_API_PATH
+    final String SURF_API_BASE = "utils/surfapi/";
+    final String SURF_API_x64 = "x64/";
+    final String SURF_API_x86 = "Win32/";
+
     // Check that Windows is being used
-    switch (SystemInfo.getCurrOS()) {
-      case WIN:
-        // Continue to architecture check
-        break;
-      default:
+    if (!Platform.isWindows()) {
         throw new IllegalStateException("System needs to be using a Windows operating system. In "
             + "order to import surfaces, this application needs run an internal DLL stored within "
             + "the JAR file, requiring a Windows OS.");
@@ -38,15 +43,12 @@ public final class ResourceLoader {
 
     // Check and determine architecture
     String apiPath = BASE_RESOURCE_PATH + SURF_API_BASE;
-    switch (SystemInfo.getCurrArch()) {
-      case x64 -> apiPath += SURF_API_x64;
-      case x86 -> apiPath += SURF_API_x86;
-      default -> {
-        System.err.println("Could not detect system architecture when initializing SurfAPI used for"
-            + "surface importing. Defaulting to 32-bit (x86) architecture.");
-        apiPath += SURF_API_x86;
-      }
+    if(Platform.is64Bit()) {
+      apiPath += SURF_API_x64;
+    } else {
+      apiPath += SURF_API_x86;
     }
+
     // Assign final value to API path
     SURF_API_PATH = apiPath;
   }
